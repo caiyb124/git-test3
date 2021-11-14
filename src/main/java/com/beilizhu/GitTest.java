87389eba039fa905f129572c76c375b58b69c9fc
beilizhu <beilizhu@qq.com>
2021-11-14 22:36:21 +0800

    git version1

---
+++ src/main/java/com/beilizhu/GitTest.java
@@ -0,0 +1,11 @@
+package com.beilizhu;
+
+/**
+ * @author shkstart
+ * @create 2021-11-14 22:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git1");
+    }
+}
