058cbc03752556cab7bfce364027b210b9a42a03
caiyb124 <1461060227@qq.com>
2021-11-14 23:31:51 +0800

    test push1

--- src/main/java/com/beilizhu/GitTest.java
+++ src/main/java/com/beilizhu/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git1");
         System.out.println("hello git2");
+        System.out.println("test push");
     }
 }
